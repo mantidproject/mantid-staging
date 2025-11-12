ceae7535cf35c2a1dc3df4cf09afc2b4dc534436
Waruna Priyankara J A Wickramasingha <142818102+warunawickramasingha@users.noreply.github.com>
2025-11-12 13:16:54 +0000

    test

--- Framework/WorkflowAlgorithms/src/AlignAndFocusPowder.cpp
+++ Framework/WorkflowAlgorithms/src/AlignAndFocusPowder.cpp
@@ -5,7 +5,7 @@
 //   Institut Laue - Langevin & CSNS, Institute of High Energy Physics, CAS
 // SPDX - License - Identifier: GPL - 3.0 +
 
-//test comment updated
+//test comment updated 2
 
 #include "MantidWorkflowAlgorithms/AlignAndFocusPowder.h"
 #include "MantidAPI/AnalysisDataService.h"
